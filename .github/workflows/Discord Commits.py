8057a297349aff893a8b376f9aeb1354ec087689
lugovigo <156248732+lugovigo@users.noreply.github.com>
2025-04-16 23:24:03 -0300

    Update Discord Commits.py

--- .github/workflows/Discord Commits.py
+++ .github/workflows/Discord Commits.py
@@ -1,7 +1,7 @@
 name: Discord
-on:
-  - push
 
+on:
+  push:
 
 jobs:
   discord:
@@ -13,4 +13,4 @@ jobs:
         with:
           webhook: ${{ secrets.DISCORD_WEBHOOK }}
           template: "avatar-with-link"
-          include-extras: true
+          include-extras: true 
