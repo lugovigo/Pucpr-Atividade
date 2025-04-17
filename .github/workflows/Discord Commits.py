c3579f037ce6532d4c31e1577663485ba7a97171
lugovigo <156248732+lugovigo@users.noreply.github.com>
2025-04-16 23:18:09 -0300

    Update Discord Commits.py

---
+++ .github/workflows/Discord Commits.py
@@ -0,0 +1,15 @@
+name: Discord
+on:
+  - push
+
+jobs:
+  discord:
+    runs-on: windows-latest
+    steps:
+      - uses: actions/checkout@v4
+      - name: Send Discord Notification
+        uses: Sniddl/discord-commits@v1.6
+        with:
+          webhook: ${{ secrets.DISCORD_WEBHOOK }}
+          template: "avatar-with-link"
+          include-extras: true
